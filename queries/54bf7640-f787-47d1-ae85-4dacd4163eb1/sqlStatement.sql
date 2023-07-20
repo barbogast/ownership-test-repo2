8818f3c4b18f3b65dc6e45ea8823812c1ab95a99
Mr. Test <mrtest@example.com>
2023-07-20 19:50:45 +0200

    Change file

---
+++ queries/54bf7640-f787-47d1-ae85-4dacd4163eb1/sqlStatement.sql
@@ -0,0 +1,9 @@
+select residents.time, residents, nonresidents from (
+    
+    select sum(value) as residents, time from debt where country = 'greece' and category_2 = 'Residents' group by time
+) as residents
+
+join (
+    select sum(value) as nonresidents, time from debt where country = 'greece' and category_2 = 'Non-residents' group by time
+) as nonresidents on residents.time = nonresidents.time
+
